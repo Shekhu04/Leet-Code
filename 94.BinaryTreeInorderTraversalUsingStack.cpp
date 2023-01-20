e79e4a98b614aeb72bdda0fd6fcfa0e969509530
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-01-20 12:20:29 +0530

    Create 94.BinaryTreeInorderTraversalUsingStack.cpp

---
+++ 94.BinaryTreeInorderTraversalUsingStack.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        stack<TreeNode*>S;
+        if(root) S.push(root);
+
+        while(! S.empty()){
+            TreeNode* temp = S.top();
+            if(temp -> left){
+                S.push(temp->left);
+                temp -> left = NULL;
+            }
+            else{
+                ans.push_back(temp ->val);
+                S.pop();
+                if(temp -> right) S.push(temp -> right);
+            }
+        }
+        return ans;
+        
+    }
+};
