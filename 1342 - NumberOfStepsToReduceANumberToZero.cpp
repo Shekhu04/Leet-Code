00f65c639ca6e32c3bbf8602015588fe52905396
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-03-13 23:33:10 +0530

    Create 1342 - NumberOfStepsToReduceANumberToZero.cpp

---
+++ 1342 - NumberOfStepsToReduceANumberToZero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        if(num<=1)
+        { 
+        return num;
+        }
+        int count=0;
+        if(num%2==0){
+       count += numberOfSteps(num/2)+1;
+    }
+    else{
+        count += numberOfSteps(num-1)+1;
+    }
+    return count;
+    }
+        
+};
