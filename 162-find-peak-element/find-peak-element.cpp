496366c881c65471b7925eefaabf36793c75e152
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-12-09 22:42:30 +0530

    Time: 3 ms (66.51%) | Memory: 9.2 MB (72.04%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-
+   
+      
         int n = nums.size();
+
+        /*brute force approach
 
         for(int i = 0; i<=n; i++) 
         {
@@ -11,8 +14,34 @@ public:
                           return i;
                           
         }
-return -1;
-        
-        
+        */
+
+        //optimal approach using binary search
+
+      //edge cases
+       if (n==1) return 0; //single element in array
+       if(nums[0] > nums[1]) return 0; //first element is the peak element
+       if(nums[n-1] > nums[n-2]) return n-1; //last element is the peak
+
+       int low=1,high=n-2;
+       while(low <= high)
+     {
+        int mid = low + (high-low)/2;
+
+        //if arr[mid] is the peak
+        if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+
+        //if we are on the left
+        if(nums[mid] < nums[mid+1]) 
+            //eliminate left half 
+            low = mid+1;
+
+        //if we are on the right 
+        //Or mid is the common point
+        else 
+           //eliminate right half
+           high = mid -1;
+     }
+     return -1;   
     }
 };
