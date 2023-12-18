ad1d9932ec49cc8eafc3b0b43bf1b9955873e286
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-12-18 22:05:54 +0530

    Time: 164 ms (45.19%) | Memory: 58.3 MB (41.32%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,36 @@
+bool canWePlace(vector<int> &stalls, int dist, int cows){
+    int n = stalls.size(); //size of array
+    int cntCows = 1; //no. of cows placed
+    int last = stalls[0]; //position of last placed cow
+    for(int i=1; i<n; i++){
+        if(stalls[i] - last >= dist){
+            cntCows++; //place next cow
+            last = stalls[i]; //update the last location
+        }
+        if(cntCows >= cows) return true;
+    }
+    return false;
+}
+
+
+class Solution {
+public:
+
+    int maxDistance(vector<int>& position, int m) {
+       int n = position.size();
+    sort(position.begin(),position.end());
+
+    int low = 0, high = position[n-1] - position[0];
+
+    //apply binary search
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(canWePlace(position,mid,m) == true){
+            low =  mid+1;
+            //ans = mid;
+        }
+        else high = mid-1;
+    }
+    return high; //ans
+    }
+};
