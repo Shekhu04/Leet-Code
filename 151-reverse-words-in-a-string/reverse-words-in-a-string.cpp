84fb7272f5323ab391edfad3749fab358b1790a6
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-03-03 12:54:57 +0530

    Time: 3 ms (84.24%) | Memory: 23 MB (23.86%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,16 +1,35 @@
+/* 1.Iterate over the string and check
+2. If there are characters assign them to temp and move forward
+3. If space is encountered then break from above loop
+4. Now check if temp size>0
+5. If ans size is 0 then assign temp to the ans
+    else do ans = temp+" "+ans
+6.If space is encountered just move the pointer forward */
+
 class Solution {
-public:
-    string reverseWords(string s) {
-        vector<string>v;
-        istringstream in(s); 
-        string word;
-        while(in>>word){
-            v.push_back(word);
-        }
-        string ans;
-        for(int i=v.size()-1;i>=0;i--){
-            if (i != v.size() - 1) ans += " ";
-            ans += v[i];
+    public:
+    string reverseWords(string s) 
+    {
+        int n = s.size();
+        int i = 0;
+        string ans="";
+
+        while(i<n){
+            string temp="";
+            while(s[i]==' ' && i<n)
+               i++;
+            while(s[i] !=' ' && i<n){
+                temp += s[i];
+               i++;
+            }
+            if(temp.size()>0)
+            {
+                if(ans.size() == 0)
+                   ans = temp;
+                   else
+                   ans = temp+" "+ans;
+            }
+              
         }
         return ans;
     }
