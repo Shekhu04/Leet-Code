e440c4dcf3dec84ecf32bf7bd3b9e13215e86ed3
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-04-20 14:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (9.07%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -13,16 +13,17 @@ class Solution {
 public:
     int sumNumbers(TreeNode* root, string number = "") {
         if (!root)
-            return 0;
+           return 0;
 
         number += std::to_string(root->val);
 
         if (!root->left && !root->right)
             return stoi(number);
 
-        int result1 = sumNumbers(root->left, number);
-        int result2 = sumNumbers(root->right, number);
+        int res1 = sumNumbers(root->left, number);
+        int res2 = sumNumbers(root->right, number);
 
-        return result1 + result2;
+        return res1 + res2;
     }
-};
+}
+    ;
