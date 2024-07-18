a9cec5bcf9fe348aad7a13ea07b866b45d2668f4
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-07-18 13:07:48 +0530

    Time: 30 ms (38.61%) | Memory: 16.4 MB (22.14%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
+    // Function to find the largest odd number in a given string of digits
     string largestOddNumber(string num) {
-        int n = num.length();
+        int n = num.length(); // Get the length of the input string
 
-        for (int i=n-1; i>=0; i--){
-            if((num[i] - '0') % 2 != 0){
-                return num.substr(0,i+1);
+        // Traverse the string from the end to the beginning
+        for (int i = n - 1; i >= 0; i--) {
+            // Check if the current digit (num[i]) is odd
+            if ((num[i] - '0') % 2 != 0) {
+                // If it's odd, return the substring from the start to this digit (inclusive)
+                return num.substr(0, i + 1);
             }
         }
+
+        // If no odd digit is found, return an empty string
         return "";
     }
 };
