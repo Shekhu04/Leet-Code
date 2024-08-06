7961ce5bc35e4baf5e47f477df8e2a762de8d6b5
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-08-06 12:10:15 +0530

    Time: 25 ms (46.59%) | Memory: 22.9 MB (43.49%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& inter) {
-        vector<vector<int>> ans;
-        int n=inter.size();
-        if(n==0)
-            return ans;
-        sort(inter.begin(),inter.end()); //sort array
-        ans.push_back(inter[0]);
-        int j=0;
-        for(int i=1;i<n;i++){
-            //if next interval merge with previous
-            if(ans[j][1]>=inter[i][0]){
-                //continue till we get the overlapping intervals
-                ans[j][1]=max(ans[j][1],inter[i][1]);
-            }
-            else {
-                j++;
-                ans.push_back(inter[i]);
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();  // Get the number of intervals
+
+        // Sort the intervals based on the starting time.
+        // This is necessary to make sure that any overlapping intervals are adjacent.
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;  // Vector to store the merged intervals
+
+        // Iterate through each interval
+        for(int i = 0; i < n; i++) {
+            // If the result array is empty or the current interval does not overlap
+            // with the last interval in the result array, add the current interval to the result
+            if(ans.empty() || intervals[i][0] > ans.back()[1]) {
+                ans.push_back(intervals[i]);
+            } else {
+                // If the current interval overlaps with the last interval in the result array,
+                // merge them by updating the end time of the last interval in the result array
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
         }
+
+        // Return the merged intervals
         return ans;
     }
 };
