06b777c6d4d5a3228ffd3f1a9e29e76e237f9b6c
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-08-08 11:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (22.24%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,23 +2,29 @@ class Solution {
 public:
     double myPow(double x, int n) {
         
-        if(n < 0) {
-            x = 1 / x;
-        } 
-        
-        long num = labs(n);
-        
-        double pow = 1;
-        
-        while(num){ // equivalent to while(num != 0)
-            if(num & 1) { // equivalent to if((num & 1) != 0)
-                pow *= x;
-            }
-            
-            x *= x;
-            num >>= 1;
-        }
-        
-        return pow;
+       double ans = 1; // Initialize the result variable to 1
+       long long num = n; // Use a long long to handle the case where n is INT_MIN
+
+       // If n is negative, convert it to positive
+       if (num < 0) num = -1 * num;
+
+       // Iterate until num becomes 0
+       while(num){
+           // If num is odd, multiply the result by x
+           if(num % 2){
+               ans = ans * x;
+               num = num - 1;
+           }
+           // If num is even, square x and divide num by 2
+           else{
+               x = x * x;
+               num = num / 2;
+           }
+       }
+
+       // If the original n was negative, invert the result
+       if (n < 0) ans = (double)(1) / (double)(ans);
+
+       return ans; // Return the final result
     }
 };
