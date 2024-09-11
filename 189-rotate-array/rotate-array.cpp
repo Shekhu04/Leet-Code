37ce83bf1d3fb4b3b9452a97c92aceb67c19150c
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-09-11 12:02:30 +0530

    Time: 18 ms (76.53%) | Memory: 27.6 MB (83.28%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,17 +1,27 @@
-
 class Solution {
 public:
+
+    // Function to reverse the array between start and end indices
+    void reverseArray(vector<int>& nums, int start, int end){
+        while(start < end){
+            swap(nums[start], nums[end]);
+            start++;
+            end--;
+        }
+    }
+
+    // Function to rotate the array
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n; // Ensure k is within the range [0, n)
+        k = k % n;  // In case k is greater than the size of the array
 
         // Reverse the entire array
-        reverse(nums.begin(), nums.end());
-        
+        reverseArray(nums, 0, n-1);
+
         // Reverse the first k elements
-        reverse(nums.begin(), nums.begin() + k);
-        
-        // Reverse the rest of the elements after k
-        reverse(nums.begin() + k, nums.end());
+        reverseArray(nums, 0, k-1);
+
+        // Reverse the remaining n-k elements
+        reverseArray(nums, k, n-1);
     }
 };
