cf63f63661aee47f74771ba0ea45ddfe73547c4e
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-09-11 11:37:44 +0530

    Time: 3 ms (96.35%), Space: 21.4 MB (60.47%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -7,7 +7,7 @@ public:
         for(int j = 1; j < nums.size(); j++){
             if(nums[j] != nums[i]){ //Checking for next unique number
 
-                nums[i+1] = nums[j]; //If unique number id found then place it just after i
+                nums[i+1] = nums[j]; //If unique number is found then place it just after i
 
                 i++; //increment i to check for future unique element
             }
