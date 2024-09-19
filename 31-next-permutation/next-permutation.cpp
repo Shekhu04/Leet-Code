adf842ac8d2525eca859555f92dae45f22f2f6ef
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-09-19 10:17:49 +0530

    Time: 4 ms (56.80%) | Memory: 14.7 MB (83.26%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int ind = -1;
+        int n = nums.size();
+
+        //Longer Prefix match
+        for(int i = n-2; i >= 0 ; i--){
+            if(nums[i] < nums[i+1]){  //breakout point
+                ind = i;
+                break;
+            }
+        }
+
+        //if there is no breakout point
+        if(ind == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        //find next greater element and swa[]
+        for(int i = n-1; i > ind; i--){
+            if(nums[i] > nums[ind] ){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        
+        //reverse the remaining array
+        reverse(nums.begin()+ind+1, nums.end());
     }
 };
