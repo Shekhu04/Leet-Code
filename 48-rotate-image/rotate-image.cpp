1a6115edd2933a0d2cb076c4925832f39323b266
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-09-20 09:45:58 +0530

    Time: 4 ms (39.54%) | Memory: 8.9 MB (84.79%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -7,7 +7,7 @@ void rotate(vector < vector < int >> & matrix) {
     //transposing the matrix
     for (int i = 0; i < n; i++) 
     {
-      for (int j = 0; j < i; j++)
+      for (int j = 0; j <= i; j++)
        {
         swap(matrix[i][j], matrix[j][i]);
        }
