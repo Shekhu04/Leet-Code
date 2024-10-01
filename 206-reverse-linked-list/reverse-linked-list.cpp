d35f3c8fd237d145efb85bdce96a4a4b014de7c0
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-10-01 11:02:39 +0530

    Time: 4 ms (76.03%) | Memory: 13 MB (78.83%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,13 +11,34 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head == NULL || head -> next == NULL){
-            return head;
-        }
-        ListNode* newHead = reverseList(head->next);
-        ListNode* front = head->next;
-        front -> next = head;
-        head->next = NULL;
-        return newHead;
+    //BFA using stack
+    //    ListNode * temp = head;
+    //    stack<int> st;
+
+    //    while(temp){
+    //     st.push(temp -> val);
+    //     temp = temp -> next;
+    //    } 
+    //    temp = head;
+
+    //    while(temp){
+    //     temp -> val = st.top();
+    //     st.pop();
+    //     temp = temp -> next;
+    //    }
+
+    //    return head;
+
+    //OA (Iterative)
+    ListNode* temp = head;
+    ListNode * prev = NULL;
+
+    while(temp){
+        ListNode* front = temp -> next;
+        temp -> next = prev;
+        prev = temp;
+        temp = front;
+    }
+    return prev;
     }
 };
