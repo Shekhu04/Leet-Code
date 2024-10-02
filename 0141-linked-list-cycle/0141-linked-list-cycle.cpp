cc1e68ef94b913130dad4fb0d069a4dbe17be9be
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-10-02 13:34:41 +0530

    Time: 7 ms (85.15%), Space: 10.9 MB (66.92%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -17,31 +17,33 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
 
-        //BFA
-        ListNode * temp = head;
+        // //BFA
+        // ListNode * temp = head;
 
-        unordered_map<ListNode*, int> nodeMap;
+        // unordered_map<ListNode*, int> nodeMap;
 
-        while(temp){
-            if(nodeMap.find(temp) != nodeMap.end()){
-                return true;
-            }
-            nodeMap[temp] = 1;
-            temp = temp -> next;
-        }
-        return false;
-       
-        // ListNode *fast = head;
-        // ListNode *slow = head;
-        // while(fast != NULL && fast ->next != NULL)
-        // {
-        //     fast = fast->next->next;
-        //     slow = slow->next;
-            
-	
-        //     if(fast == slow)
+        // while(temp){
+        //     if(nodeMap.find(temp) != nodeMap.end()){  //nodeMap.end() => not present
         //         return true;
+        //     }
+        //     nodeMap[temp] = 1;
+        //     temp = temp -> next;
         // }
         // return false;
+
+
+       //OA
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+	
+            if(fast == slow)
+                return true;
+        }
+        return false;
     }
 };
