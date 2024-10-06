d6952bca4868b911117399b775374c2755658b32
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-10-06 13:10:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (74.21%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,25 +1,31 @@
-
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-       string ans = "";
-       int count = 0;
-       for(char ch:s) {
-           if(ch=='(' && count == 0){
-               count++;
-           }
-           else if(ch=='(' && count >= 1){
-               ans = ans + ch;
-               count++;
-           }
-           else if(ch == ')' && count > 1){
-               ans = ans+ch;
-               count--;
-           }
-           else if(ch==')' && count == 1){
-               count --;
-           }
-       }
-       return ans; 
+        int count = 0;
+        string ans ="";
+
+        for (int i = 0; i < s.length(); i++) {
+        char c = s[i];
+
+        // Check for '(' when count is 0 (first opening parenthesis of an outer group)
+        if (c == '(' && count == 0) {  
+            count++;
+        }
+        // For subsequent '(' when count is 1 or more, add to answer
+        else if (c == '(' && count >= 1) {
+            ans += c;
+            count++;
+        }
+        // For ')' that closes an inner group, add to answer if count is greater than 1
+        else if (c == ')' && count > 1) {
+            ans += c;
+            count--;
+        }
+        // For ')' that closes an outer group
+        else if (c == ')' && count == 1) {
+            count--;
+        }
+      }
+        return ans;
     }
-};
+};  
