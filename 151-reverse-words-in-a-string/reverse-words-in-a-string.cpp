081f5d2dbd72a6a83c9a5a36e1b2354065ca13c5
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-10-07 10:27:11 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (15.21%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,36 +1,36 @@
-/* 1.Iterate over the string and check
-2. If there are characters assign them to temp and move forward
-3. If space is encountered then break from above loop
-4. Now check if temp size>0
-5. If ans size is 0 then assign temp to the ans
-    else do ans = temp+" "+ans
-6.If space is encountered just move the pointer forward */
-
 class Solution {
-    public:
-    string reverseWords(string s) 
-    {
-        int n = s.size();
-        int i = 0;
-        string ans="";
+public:
+    string reverseWords(string s) {
+        int n = s.size(); // Get the length of the input string
+        int i = 0; // Initialize an index to traverse the string
+        string ans = ""; // Initialize the result string
 
-        while(i<n){
-            string temp="";
-            while(s[i]==' ' && i<n)
-               i++;
-            while(s[i] !=' ' && i<n){
-                temp += s[i];
-               i++;
+        // Loop until we have processed the entire string
+        while (i < n) {
+            string temp = ""; // Temporary string to hold the current word
+
+            // Skip any leading spaces
+            while (s[i] == ' ' && i < n) {
+                i++; // Move the index forward
             }
-            if(temp.size()>0)
-            {
-                if(ans.size() == 0)
-                   ans = temp;
-                   else
-                   ans = temp+" "+ans;
+
+            // Collect characters of the current word
+            while (s[i] != ' ' && i < n) {
+                temp += s[i]; // Append the current character to temp
+                i++; // Move the index forward
             }
-              
+
+            // If a non-empty word was found
+            if (temp.size() > 0) {
+                // If ans is empty, directly assign temp to it
+                if (ans.size() == 0) {
+                    ans = temp; 
+                } else {
+                    // Otherwise, prepend the current word to ans
+                    ans = temp + " " + ans; 
+                }
+            }
         }
-        return ans;
+        return ans; // Return the final reversed string
     }
 };
