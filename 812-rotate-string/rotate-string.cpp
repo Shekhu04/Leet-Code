f2394201827dc19b5ea3ddb9d33c82f458ebaf74
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-10-08 11:16:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (95.13%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        return (s.length() == goal.length() && (s+s).contains(goal));
+    int n = s.length();  // Length of the string 's'
+    int m = goal.length();  // Length of the string 'goal'
+    int i = 0;  // Counter to keep track of number of rotations
+
+    // If the lengths of 's' and 'goal' are not the same, they cannot be rotations of each other
+    if (m != n) {
+        return false;
+    }
+
+    // Try rotating the string 's' n times
+    while (i < n) {
+        // If 's' matches 'goal', return true
+        if (s == goal) {
+            return true;
+        }
+
+        // Save the first character of 's'
+        char firstChar = s[0];
+
+        // Shift all characters of 's' one position to the left
+        for (int j = 0; j < n - 1; j++) {
+            s[j] = s[j + 1];
+        }
+
+        // Place the first character at the end of the string
+        s[n - 1] = firstChar;
+
+        // Increment the counter
+        i++;
+    }
+
+    // If no rotation of 's' matches 'goal', return false
+    return false;
     }
 };
