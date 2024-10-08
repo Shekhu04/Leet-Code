16c29e497986fcea04103cc391d9a8687ab8f233
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-10-08 12:25:11 +0530

    Time: 8 ms (63.52%), Space: 9.8 MB (37.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,38 +1,37 @@
 #include <unordered_map>
-#include <string>
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) {
+        // If lengths are not equal, they cannot be anagrams
+    if (s.size() != t.size()) {
+        return false;
+    }
+
+    // Use unordered_map to store character frequencies
+    unordered_map<char, int> charFrequency;
+
+    // Increment frequency for each character in string 's'
+    for (int i = 0; i < s.size(); i++) {
+        charFrequency[s[i]]++;  // Increment count of s[i]
+    }
+
+    // Decrement frequency for each character in string 't'
+    for (int i = 0; i < t.size(); i++) {
+        charFrequency[t[i]]--;  // Decrement count of t[i]
+
+        // If frequency becomes negative, strings are not anagrams
+        if (charFrequency[t[i]] < 0) {
             return false;
         }
+    }
 
-        // Use unordered_map to store character frequencies
-        unordered_map<char, int> charFrequency;
-
-        // Increment frequency for each character in string s
-        for (char c : s) {
-            charFrequency[c]++;
+    // Check if all character frequencies are zero
+    for (auto it = charFrequency.begin(); it != charFrequency.end(); ++it) {
+        if (it->second != 0) {
+            return false;
         }
+    }
 
-        // Decrement frequency for each character in string t
-        for (char c : t) {
-            charFrequency[c]--;
-
-            // If frequency becomes negative, strings are not anagrams
-            if (charFrequency[c] < 0) {
-                return false;
-            }
-        }
-
-        // Check if all character frequencies are zero
-        for (const auto& pair : charFrequency) {
-            if (pair.second != 0) {
-                return false;
-            }
-        }
-
-        return true;
+    return true;
     }
 };
