591857aa0ba259ffa83f3cf901f2ec8327b52cc0
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-10-12 13:04:44 +0530

    Time: 287 ms (10.64%), Space: 30.5 MB (11.12%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int ans = 0; // Variable to store the total beauty sum
-        int n = s.size(); // Length of the input string
+        int ans = 0;
+        int n = s.size();
 
-        // Loop to consider every possible starting point of a substring
-        for(int i = 0; i < n; i++){
-            map<char, int> m; // Map to store the frequency of characters in the current substring
+        for(int i = 0; i<n; i++){
+            map<char,int> m;
 
-            // Loop to consider every possible ending point of the substring starting at i
             for(int j = i; j < n; j++){
-                m[s[j]]++; // Increment the frequency of the current character
-                int max_freq = 0; // Variable to store the maximum frequency in the current substring
-                int least_freq = INT_MAX; // Variable to store the minimum frequency in the current substring
+                m[s[j]]++;
 
-                // Loop through the frequency map to find max and min frequencies
-                for(auto a : m){
-                    max_freq = max(max_freq, a.second);
-                    least_freq = min(least_freq, a.second);
+                int max_freq = 0;
+                int min_freq = INT_MAX;
+
+                for(auto it : m){
+                    max_freq = max(max_freq, it.second);
+                    min_freq = min(min_freq, it.second);
                 }
-
-                // Add the difference between max and least frequency to the answer
-                ans += (max_freq - least_freq);
+            ans += (max_freq - min_freq);
             }
         }
-        return ans; // Return the total beauty sum
+        return ans;
     }
 };
